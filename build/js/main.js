cca00192564fd89adb6490fb302f07f206026219
BPS.TX02 <luisleonardomartinez375@live.com>
2023-06-05 12:50:09 -0500

    lesson 2 finished

--- build/js/main.js
+++ build/js/main.js
@@ -1,8 +1,19 @@
 "use strict";
-let username = 'luis martinez';
-console.log(username);
-let a = 12;
-let b = 6;
-let c = 2;
-console.log(a / b);
-console.log(c * b);
+//string, number, bool, any
+let myName;
+let meaningOfLife;
+let isLoading;
+let album;
+myName = 'Luis';
+meaningOfLife = 42;
+isLoading = true;
+album = 5150;
+//function
+const sum = (a, b) => {
+    return a + b;
+};
+//union types
+let postId;
+let isActive;
+//inference example
+let re = /\W+/g;
