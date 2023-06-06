a512fd342720a67af1d85d67386502e5a1e5c9f2
BPS.TX02 <luisleonardomartinez375@live.com>
2023-06-06 16:39:18 -0500

    finshed lesson09

--- build/js/main.js
+++ build/js/main.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 // arrays
 //string array
 let stringArr = ['one', 'hey', 'Dave'];
@@ -461,3 +470,60 @@ store.state = 'John';
 const myState = new StateObject([15]);
 myState.state = (['Dave', 41, true]);
 console.log(myState.state);
+const updateAssignment = (assign, propsToUpdate) => {
+    return Object.assign(Object.assign({}, assign), propsToUpdate);
+};
+const assign1 = {
+    studentId: "compsci123",
+    title: "final project",
+    grade: 0
+};
+console.log(updateAssignment(assign1, { grade: 95 }));
+const assignGraded = updateAssignment(assign1, { grade: 95 });
+//Required and readonly
+const recordAssignment = (assign) => {
+    return assign;
+};
+const assignVerified = Object.assign(Object.assign({}, assignGraded), { verified: true });
+recordAssignment(Object.assign(Object.assign({}, assignGraded), { verified: true }));
+const hexColorMap = {
+    red: "FF0000",
+    green: "00FF00",
+    blue: "0000FF",
+};
+const finalGrades = {
+    sara: "B",
+    kelly: "U"
+};
+const gradeData = {
+    sara: { assign1: 85, assign2: 93 },
+    kelly: { assign1: 76, assign2: 15 }
+};
+const score = {
+    studentId: "k124",
+    grade: 85
+};
+const preview = {
+    studentId: "k123",
+    title: "Final Project"
+};
+// returnType
+// type newAssign = {title: string, points: number}
+const createNewAssign = (title, points) => {
+    return { title, points };
+};
+const tsAssign = createNewAssign("utility types", 100);
+console.log(tsAssign);
+const assgignArgs = ["Generics", 100];
+const tsAssign2 = createNewAssign(...assgignArgs);
+console.log(tsAssign2);
+const fetchUsers = () => __awaiter(void 0, void 0, void 0, function* () {
+    const data = yield fetch('https://jsonplaceholder.typicode.com/users').then(res => {
+        return res.json();
+    }).catch(err => {
+        if (err instanceof Error)
+            console.log(err.message);
+    });
+    return data;
+});
+fetchUsers().then(users => console.log(users));
--- src/main.ts
+++ src/main.ts
@@ -655,3 +655,140 @@ const myState = new StateObject<(string | number | boolean)[]>([15])
 
 myState.state = (['Dave',41, true])
 console.log(myState.state)
+
+//////////////////////////////////////////////////
+//utility types
+
+//partial
+interface Assignment {
+    studentId: string,
+    title: string,
+    grade: number,
+    verified?: boolean
+} 
+
+const updateAssignment = (assign: Assignment, propsToUpdate: Partial<Assignment>): Assignment => {
+    return{ ...assign, ...propsToUpdate}
+}
+
+const assign1: Assignment = {
+    studentId: "compsci123",
+    title: "final project",
+    grade: 0
+}
+console.log(updateAssignment(assign1, {grade:95}))
+const assignGraded: Assignment = updateAssignment(assign1, {grade:95})
+
+//Required and readonly
+
+const recordAssignment = (assign: Required<Assignment>): Assignment =>{
+    return assign
+}
+
+const assignVerified: Readonly<Assignment> = {
+    ... assignGraded, verified:true
+
+}
+
+recordAssignment({...assignGraded, verified:true})
+
+const hexColorMap: Record<string, string> = {
+    red: "FF0000",
+    green: "00FF00",
+    blue: "0000FF",
+}
+
+type Students = "sara" | "kelly"
+
+type LetterGrades = "A" | "B" | "C" | "D" | "U"
+
+const finalGrades: Record<Students, LetterGrades> = {
+    sara: "B",
+    kelly: "U"
+}
+
+interface Grades{
+    assign1: number,
+    assign2: number,
+}
+
+const gradeData: Record<Students,Grades> ={
+    sara: {assign1:85, assign2: 93},
+    kelly: {assign1: 76, assign2 : 15}
+
+}
+//Pick and omit
+
+type AssignResult =  Pick<Assignment, "studentId" | "grade">
+
+const score: AssignResult = {
+    studentId: "k124",
+    grade: 85
+}
+
+type AssignPrev = Omit<Assignment, "grade" | "verified">
+
+const preview: AssignPrev = {
+    studentId: "k123",
+    title: "Final Project"
+}
+
+// Exclude and extract 
+
+type adjustedGrade =  Exclude<LetterGrades, "U">
+
+type highGrades = Extract<LetterGrades, "A" | "B">
+
+//Nonnullable
+
+type AllPossibleGrades = 'Dave' |'Luis' | 'John' | null | undefined
+
+type NamesOnly = NonNullable<AllPossibleGrades>
+
+// returnType
+
+// type newAssign = {title: string, points: number}
+
+const createNewAssign = (title: string, points: number) => {
+    return {title, points}
+}
+
+type newAssign = ReturnType<typeof createNewAssign>
+
+const tsAssign: newAssign = createNewAssign("utility types", 100)
+console.log(tsAssign)
+
+//parameters 
+
+type assignParams =  Parameters<typeof createNewAssign>
+
+const assgignArgs: assignParams = ["Generics", 100]
+
+const tsAssign2: newAssign = createNewAssign(...assgignArgs)
+
+console.log(tsAssign2)
+
+//awaited  -helps us with return type of a promise
+
+interface User {
+    id: number,
+    name: string,
+    username: string,
+    email: string,
+}
+
+const fetchUsers = async (): Promise<User[]> => {
+
+    const data = await fetch(
+        'https://jsonplaceholder.typicode.com/users'
+    ).then(res => {
+        return res.json()
+    }).catch(err => {
+        if (err instanceof Error) console.log(err.message)
+    })
+    return data
+}
+
+type FetchUsersReturnType = Awaited<ReturnType<typeof fetchUsers>>
+
+fetchUsers().then(users => console.log(users))
