929a8ad0c327fa9898d4a4a845bf4c62073561b4
BPS.TX02 <luisleonardomartinez375@live.com>
2023-06-08 10:02:21 -0500

    finished lesson16

--- lesson16/src/Context/CartProvider.tsx
+++ lesson16/src/Context/CartProvider.tsx
@@ -1,4 +1,4 @@
-import { Reducer } from "react"
+import {ReactElement, createContext, useMemo, useReducer} from 'react'
 
 export type CartItemType = {
     sku: string,
@@ -46,12 +46,31 @@ const reducer = (state: CartStateType, action: ReducerAction): CartStateType => 
             if (!action.payload){
                 throw new Error('action.payload missing in REMOVE action')
             }
+            const {sku} = action.payload
+
+            const filteredCart: CartItemType[] = state.cart.filter(item => item.sku !== sku)
+            
+            return {...state, cart: filteredCart}
 
         }
         case REDUCER_ACTION_TYPE.QUANTITY: {
             if (!action.payload){
                 throw new Error('action.payload missing in QUANTITY action')
             }
+            const {sku, qty} = action.payload
+
+            // const filteredCart: CartItemType[] = state.cart.filter(item => item.sku !== sku)
+
+            const itemExists: CartItemType | undefined = state.cart.find(item => item.sku === sku)
+
+            if(!itemExists){
+                throw new Error('item not found in cart')
+            }
+
+            const updatedItem: CartItemType = {...itemExists, qty}
+
+            return {...state, cart: [...state.cart.filter(item => item.sku !== sku), updatedItem]}
+
 
         }
         case REDUCER_ACTION_TYPE.SUBMIT{
@@ -61,3 +80,43 @@ const reducer = (state: CartStateType, action: ReducerAction): CartStateType => 
 
     }
 }
+
+const useCartContext = (initCartState: CartStateType) => {
+    const [state, dispatch] = useReducer(reducer, initCartState)
+
+    const REDUCER_ACTIONS = useMemo(() =>{
+        return REDUCER_ACTION_TYPE
+    }, [])
+
+    const totalItems: number = state.cart.reduce((prevousValue, CartItem) => {
+        return prevousValue + CartItem.qty
+    }, 0)
+
+    const totalPrice = new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD'}).format(state.cart.reduce((prevousValue, CartItem) => {
+        return prevousValue + (CartItem.qty * CartItem.price)
+    }, 0)
+    )
+    const cart = state.cart.sort((a, b) => {
+        const itemA = Number(a.sku.slice(-4))
+        const itemB = Number(b.sku.slice(-4))
+        return itemA - itemB
+})
+    return {dispatch, REDUCER_ACTIONS, totalItems, totalPrice, cart}
+}
+
+export type UseCartContextType = ReturnType<typeof useCartContext>    
+
+const initCartContextState: UseCartContextType = {dispatch: () => {}, REDUCER_ACTIONS: REDUCER_ACTION_TYPE, totalItems: 0, totalPrice: '', cart: []}
+
+export const CartContext = createContext<UseCartContextType>(initCartContextState)
+
+
+type ChildrenType = { Children?: ReactElement | ReactElement[]}
+
+export const CartProvider = ({Children}: ChildrenType): ReactElement => {
+    return (<CartContext.Provider value={useCartContext(initCartState)}>
+        {Children}
+    </CartContext.Provider>)
+}
+
+export default CartContext
